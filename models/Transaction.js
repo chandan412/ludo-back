a829b0b9fa1c4cdfeb25d3e476aae8b28b7c9d1b
chandan412 <59800332+chandan412@users.noreply.github.com>
2026-06-11 21:17:10 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -4,7 +4,7 @@ const transactionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: {
     type: String,
-    enum: ['recharge', 'withdraw', 'game_win', 'game_loss', 'game_lock', 'game_unlock', 'platform_fee', 'refund'],
+    enum: ['recharge', 'withdraw', 'game_win', 'game_loss', 'game_lock', 'game_unlock', 'platform_fee', 'refund', 'referral'],
     required: true
   },
   amount: { type: Number, required: true },
